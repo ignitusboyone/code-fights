ed510d5512f4d060a88ad6dd4b01e7afb37703c5
ignitus <ignitus@email.no>
2016-09-10 23:22:21 -0400

    Layberinth and Casarian Solutions

---
+++ source/cpp/Labyrinth.cpp
@@ -0,0 +1,497 @@
+#include <vector>
+#include <deque>
+#include <string>
+#include <iostream>
+
+#include <cstdlib>
+#define _DEBUG 2
+
+enum Direction : int8_t {UP,DOWN,LEFT,RIGHT, NEUTRAL};
+#ifdef _DEBUG
+std::ostream& operator<< (std::ostream& ostr, Direction obj){
+  switch(obj){
+    case UP:    ostr<<'U';  break;
+    case DOWN:  ostr<<'D';  break;
+    case LEFT:  ostr<<'L';  break;
+    case RIGHT: ostr<<'R';  break;
+    default:          break;
+  }
+  return ostr;
+}
+#endif
+class Position {
+public:
+  Position(size_t x=0, size_t y=0)
+    :_X(x)
+    ,_Y(y)
+  {}
+  inline void setX(size_t x){ _X = x;}
+  inline void setY(size_t y){ _Y = y;}
+  inline size_t getX(){ return _X;}
+  inline size_t getY(){ return _Y;}
+  //---------------------------------------------
+  bool operator==(const Position& rhs)
+    { return _X == rhs._X && _Y == rhs._Y;}
+  //---------------------------------------------
+  Position operator+(Position& rhs)
+    { 
+      Position rvalue(_X+rhs._X, _Y+rhs._Y);
+      return rvalue;
+    }
+  //---------------------------------------------
+  Position operator-(Position& rhs)
+    { 
+      Position rvalue(_X-rhs._X, _Y-rhs._Y);
+      return rvalue;
+    }
+  //---------------------------------------------
+  Position& operator+=(Position& rhs)
+    { 
+      _X+=rhs._X;
+      _Y+=rhs._Y;
+      return *this;
+    }
+  //---------------------------------------------
+  Position& operator-=(Position& rhs)
+    { 
+      _X-=rhs._X;
+      _Y-=rhs._Y;
+      return *this;
+    }
+  //---------------------------------------------
+  void move(const Direction& rhs)
+    { 
+      switch (rhs){
+        case UP:    _Y-=1;  break;
+        case DOWN:  _Y+=1;  break;
+        case LEFT:  _X-=1;  break;
+        case RIGHT: _X+=1;  break;
+        default:          break;
+      }
+    }
+
+private:
+  size_t _X;
+  size_t _Y;
+};
+//-----------------------------------------------------------------------------
+class Solution: public std::vector<Direction> {
+public:
+  Solution(std::vector<Direction> cmds)
+    :std::vector<Direction>(cmds)
+  {}
+  
+    Position getQuickStart() const { return _QuickStartPosition; }
+    void     setQuickStart(const Position& pose) { _QuickStartPosition = pose; }
+protected:
+  Position _QuickStartPosition;
+};
+//-----------------------------------------------------------------------------
+class Laybrinth {
+public:
+  Laybrinth(std::vector<std::string> map, 
+             Position initial, 
+             Position finish)
+  :_labyrinth(map)
+  ,_start(initial)
+  ,_finish(finish)
+  {}
+  
+  inline Position Start() const { return _start;};
+  inline Position Finish() const { return _finish;};
+  //---------------------------------------------
+  bool simulate(Solution& work_queue,
+                  size_t& cycles)
+  {
+    cycles = -1;
+    bool done = false;
+    Position current = _start;
+    Position prev    = _start;
+    Direction prev_cmd = NEUTRAL;
+    while(!done) {
+      auto itr = work_queue.begin();
+      for ( itr + work_queue.size()-1; 
+            itr != work_queue.end(); 
+            ++itr)
+     {
+        current.move(*itr);
+        if(current == _finish){ 
+          return true;
+        } else if ( _labyrinth[current.getY()][current.getX()] != '.' ) { 
+          done = true; break;
+        } else if ( current == prev && itr != work_queue.end()-1) {
+          done = true; break;
+        }
+        prev.move(prev_cmd);
+        prev_cmd = *itr;
+      }
+      prev.move(prev_cmd);
+      prev_cmd = NEUTRAL;
+      ++cycles;
+      if( current == _start){
+        break;
+      }
+    }
+    work_queue.setQuickStart(current);
+    return false;
+  }
+private:
+    const std::vector<std::string> _labyrinth;
+    const Position _start;
+    const Position _finish;
+};
+//-----------------------------------------------------------------------------
+int labyrinthNavigation(std::vector<std::string> map, 
+                        std::vector<int> start, 
+                        std::vector<int> dest) {
+  decltype(map)  padded_map(map.size()+2,
+                            std::string(map[1].size()+2,'#'));
+  for (auto i=0; i<map.size(); ++i)
+    padded_map[i+1].replace(1,map[i].size(),map[i]);
+ 
+  Laybrinth laybrinth(padded_map,Position(start[1]+1,start[0]+1),Position(dest[1]+1,dest[0]+1)); 
+
+#if _DEBUG > 1
+  std::cout << "Start( "<< start[1]+1<< " , " << start[0]+1 <<" )\n";
+  std::cout << "Finish( "<< dest[1]+1<< " , " <<  dest[0]+1 <<" )\n";
+
+  padded_map[(start[0]+1)][(start[1]+1)] = 'S';
+  padded_map[ (dest[0]+1)][ (dest[1]+1)] = 'F';
+  
+  for(auto s:padded_map)
+    std::cout << s << "\n";
+  std::cout << "\n";
+#endif
+    
+  std::deque<Solution> work_queue;
+  work_queue.push_back(Solution({UP}));
+  work_queue.push_back(Solution({DOWN}));
+  work_queue.push_back(Solution({LEFT}));
+  work_queue.push_back(Solution({RIGHT}));
+  
+  while (work_queue.size()){
+    auto cmd_queue = work_queue.front();
+    size_t cycles = 0;
+    if( laybrinth.simulate(cmd_queue,cycles )){
+#if _DEBUG
+      for ( auto cmd : cmd_queue )
+        std::cout << cmd << ",";
+      std::cout << "\n";
+#endif
+       return cmd_queue.size();
+    } else {
+      if (cycles ){
+#if _DEBUG > 2
+        std::cout << work_queue.size() << ": ";
+        for ( auto cmd : cmd_queue )
+          std::cout << cmd << ",";
+        std::cout << "\n";
+#endif        
+        // if (work_queue.front().size() > 11) break;
+        cmd_queue.push_back(UP);
+        work_queue.push_back(cmd_queue);
+        cmd_queue.pop_back();
+        
+        cmd_queue.push_back(DOWN);
+        work_queue.push_back(cmd_queue);
+        cmd_queue.pop_back();
+          
+        cmd_queue.push_back(LEFT);
+        work_queue.push_back(cmd_queue);
+        cmd_queue.pop_back();
+          
+        cmd_queue.push_back(RIGHT);
+        work_queue.push_back(cmd_queue);
+        cmd_queue.pop_back();
+      }
+    }
+    work_queue.pop_front();
+  }
+  return -1;
+}
+
+#include <gtest/gtest.h>
+//-----------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_1) {
+    EXPECT_EQ ( 3, labyrinthNavigation (
+      {".......",
+       "...*.*.",
+       "*....*.",
+       ".......",
+       ".**...."}
+       ,{4,6}
+       ,{0,0}
+));
+};
+
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_2) {
+    EXPECT_EQ ( 4, labyrinthNavigation (
+      {"..",
+       ".*",
+       "..",
+       ".*",
+       "..",
+       ".*",
+       "..",
+       ".*",
+       ".."}
+     ,{8,1}
+     ,{0,1}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_3) {
+    EXPECT_EQ ( 5, labyrinthNavigation (
+      {"...**...*",
+       "....*....",
+       ".......*.",
+       ".*.*...*.",
+       ".***.*...",
+       ".....*...",
+       ".***.*.*.",
+       ".*.*.***.",
+       "........."}
+     ,{6,8}
+     ,{2,0}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_4) {
+    EXPECT_EQ ( 6, labyrinthNavigation (
+      {".*.",
+       ".*.",
+       "..."}
+     ,{0,2}
+     ,{0,0}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_5) {
+    EXPECT_EQ ( 1, labyrinthNavigation (
+      {".................................................."}
+      ,{0,0}
+      ,{0,49}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_6) {
+    EXPECT_EQ ( 7, labyrinthNavigation (
+      {"...................",
+       "***.*..*.*..*.*..*."}
+     ,{1,18}
+     ,{0,0}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_7) {
+    EXPECT_EQ ( 8, labyrinthNavigation (
+      {"...",
+       ".*.",
+       ".**",
+       "..."}
+       ,{1,2}
+       ,{3,2}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_8) {
+    EXPECT_EQ ( 1, labyrinthNavigation (
+      {"*",
+       ".",
+       ".",
+       ".",
+       ".",
+       "*",
+       ".",
+       ".",
+       "*",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       ".",
+       "*",
+       ".",
+       ".",
+       "*",
+       "*",
+       "*",
+       "*"}
+     ,{42,0}
+     ,{16,0}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_9) {
+    EXPECT_EQ ( 5, labyrinthNavigation (
+      {"......*",
+       ".*.....",
+       ".**...*",
+       "......."}
+     ,{0,0}
+     ,{3,6}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_10) {
+    EXPECT_EQ ( 6, labyrinthNavigation (
+      {"...........................",
+       "...........................",
+       "...........................",
+       "...........................",
+       "...........................",
+       "...........................",
+       "..........................."}
+     ,{5,24}
+     ,{1, 0}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_11) {
+    EXPECT_EQ ( 8, labyrinthNavigation (
+      {".........**....",
+       ".*...*...*.**..",
+       ".**............",
+       "......*...*...."}
+     ,{1,3}
+     ,{3,14}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_12) {
+    EXPECT_EQ ( 4, labyrinthNavigation (
+      {"......*",
+       ".*.....",
+       ".**...*",
+       "......."}
+     ,{0,5}
+     ,{3,6}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_13) {
+    EXPECT_EQ ( 4, labyrinthNavigation (
+      {"...........*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".***.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       "..............."}
+     ,{7,0}
+     ,{6,14}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_14) {
+    EXPECT_EQ ( 6, labyrinthNavigation (
+      {"...........*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       ".*.*.*.*.*.*...",
+       "...............",
+       ".*.*.*.*.*.*...",
+       ".*............."}
+     ,{9,0}
+     ,{9,14}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_15) {
+    EXPECT_EQ ( 3, labyrinthNavigation (
+      {"...............",
+       ".*************.",
+       ".*...........*.",
+       ".*...........*.",
+       ".*...........*.",
+       ".*...........*.",
+       ".*...........*.",
+       ".*...........*.",
+       ".*************.",
+       "..............."}
+     ,{2, 2}
+     ,{6,11}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_16) {
+    EXPECT_EQ ( 32, labyrinthNavigation (
+      {"*************..",
+       "...............",
+       "..*************",
+       "...............",
+       "*************..",
+       "..............",
+       "..*************",
+       "...............",
+       "*************..",
+       "..............."}
+     ,{0,14}
+     ,{9,0}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_17) {
+    EXPECT_EQ ( 3, labyrinthNavigation (
+      {"...........................",
+       "...........................",
+       "...........................",
+       "...........................",
+       "...........................",
+       "...........................",
+       "..........................."}
+     ,{0,0}
+     ,{6,13}
+));
+};
+//-------------------------------------------------------------------------------
+TEST (labyrinthNavigation, Test_18) {
+    EXPECT_EQ ( 32, labyrinthNavigation (
+      {"************************..",
+       "..........................",
+       "..************************",
+       "..........................",
+       "************************..",
+       ".........................",
+       "..************************",
+       "..........................",
+       "************************..",
+       ".........................."}
+     ,{0,24}
+     ,{9,0}
+));
+};
--- source/cpp/caesarian.cpp
+++ source/cpp/caesarian.cpp
@@ -1,5 +1,57 @@
-auto caesarian(auto m, int n) {
-    for (i : m)
+#include <string>
+
+std::string caesarian(std::string m, int n) {
+    for ( auto& i : m)
         i = (i + 7 + n % 26) % 26 + 97;
     return m;
 }
+
+#include <gtest/gtest.h>
+//-----------------------------------------------------------------------------
+TEST (caesarian, abc) {
+    EXPECT_EQ ( "def", caesarian (
+      "abc",3
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, egg) {
+    EXPECT_EQ ( "dff", caesarian (
+      "egg",-1
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, super) {
+    EXPECT_EQ ( "tvqfs", caesarian (
+      "super",27
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, internationally) {
+    EXPECT_EQ ( "joufsobujpobmmz", caesarian (
+      "internationally",12039
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, stargazing) {
+    EXPECT_EQ ( "lmtkztsbgz", caesarian (
+      "stargazing",-3023
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, polarequation) {
+    EXPECT_EQ ( "febqhugkqjyed", caesarian (
+      "polarequation",16
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, codefights) {
+    EXPECT_EQ ( "pbqrsvtugf", caesarian (
+      "codefights",-1001
+));
+};
+//-----------------------------------------------------------------------------
+TEST (caesarian, caesarian) {
+    EXPECT_EQ ( "aycqypgyl", caesarian (
+      "caesarian",2000000000
+));
+};
